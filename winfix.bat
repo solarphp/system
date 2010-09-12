16bebc3cd7531aa9ee9925e949d8f006cf6889d5
Paul M. Jones <pmjones@solarphp.com>
2010-09-12 22:24:44 +0000

    add fix for docroot/public symlinks

--- winfix.bat
+++ winfix.bat
@@ -1,4 +1,4 @@
-﻿cd script
+cd script
 del solar
 mklink solar ..\source\solar\script\solar
 cd ..
@@ -7,23 +7,27 @@ cd include
 del Solar.php
 mklink Solar.php ..\source\solar\Solar.php
 del Solar
-mklink /D Solar ..\source\solar\Solar
+mklink \D Solar ..\source\solar\Solar
 cd ..
 
-cd include/Test
+cd include\Test
 del Solar.php
 mklink Solar.php ..\..\source\solar\tests\Test\Solar.php
 del Solar
-mklink /D Solar ..\..\source\solar\tests\Test\Solar
+mklink \D Solar ..\..\source\solar\tests\Test\Solar
 cd ..\..
 
-cd include/Mock
+cd include\Mock
 del Solar
-mklink /D Solar ..\..\source\solar\tests\Mock\Solar
+mklink \D Solar ..\..\source\solar\tests\Mock\Solar
 cd ..\..
 
-cd include/Fixture
+cd include\Fixture
 del Solar
-mklink /D Solar ..\..\source\solar\tests\Fixture\Solar
+mklink \D Solar ..\..\source\solar\tests\Fixture\Solar
 cd ..\..
 
+cd docroot\public\Solar\View\Helper
+del Pager
+mklink Pager ..\..\..\..\..\include\Solar\View\Helper\Pager\Public
+cd ..\..\..\..\..

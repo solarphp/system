d3d5ad3cdf05cdfca36d6b9f465b11ad941ac7ca
Paul M. Jones <pmjones@solarphp.com>
2010-09-12 22:13:48 +0000

    use del, not rmdir, since svn places the symlinks as files, not directories

--- winfix.bat
+++ winfix.bat
@@ -1,6 +1,4 @@
-﻿REM must run as administrator
-
-cd script
+﻿cd script
 del solar
 mklink solar ..\source\solar\script\solar
 cd ..
@@ -8,27 +6,24 @@ cd ..
 cd include
 del Solar.php
 mklink Solar.php ..\source\solar\Solar.php
-rmdir Solar
+del Solar
 mklink /D Solar ..\source\solar\Solar
 cd ..
 
 cd include/Test
 del Solar.php
 mklink Solar.php ..\..\source\solar\tests\Test\Solar.php
-rmdir Solar
+del Solar
 mklink /D Solar ..\..\source\solar\tests\Test\Solar
 cd ..\..
 
 cd include/Mock
-rmdir Solar
+del Solar
 mklink /D Solar ..\..\source\solar\tests\Mock\Solar
 cd ..\..
 
 cd include/Fixture
-rmdir Solar
+del Solar
 mklink /D Solar ..\..\source\solar\tests\Fixture\Solar
 cd ..\..
-
-REM still need to link public resources.
-REM can we do that with `php script\solar link-public Solar` at this point?
 

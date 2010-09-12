6a1c84c914e7f9a86b6f78449c5dc4d4b5199a6f
Paul M. Jones <pmjones@solarphp.com>
2010-09-12 21:59:39 +0000

    add a script to fix symlinks on windows

---
+++ winfix.bat
@@ -0,0 +1,34 @@
+﻿REM must run as administrator
+
+cd script
+del solar
+mklink solar ..\source\solar\script\solar
+cd ..
+
+cd include
+del Solar.php
+mklink Solar.php ..\source\solar\Solar.php
+rmdir Solar
+mklink /D Solar ..\source\solar\Solar
+cd ..
+
+cd include/Test
+del Solar.php
+mklink Solar.php ..\..\source\solar\tests\Test\Solar.php
+rmdir Solar
+mklink /D Solar ..\..\source\solar\tests\Test\Solar
+cd ..\..
+
+cd include/Mock
+rmdir Solar
+mklink /D Solar ..\..\source\solar\tests\Mock\Solar
+cd ..\..
+
+cd include/Fixture
+rmdir Solar
+mklink /D Solar ..\..\source\solar\tests\Fixture\Solar
+cd ..\..
+
+REM still need to link public resources.
+REM can we do that with `php script\solar link-public Solar` at this point?
+

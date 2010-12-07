c356530f273a7db260a5551f0ef584fd6b67f0ac
Paul M. Jones <pmjones88@gmail.com>
2010-12-06 20:48:11 -0600

    remove accidental proxima entries

--- config.php
+++ config.php
@@ -1,69 +1,40 @@
 <?php
 /**
- * all config values go in this array, which will be returned at the end of
- * this script. vendor configs should modify this array directly.
- */
+* all config values go in this array, which will be returned at the end of
+* this script. vendor configs should modify this array directly.
+*/
 $config = array();
 
 /**
- * system directory
- */
+* system directory
+*/
 $system = dirname(__FILE__);
 $config['Solar']['system'] = $system;
 
 /**
- * default configs for each vendor
- */
+* default configs for each vendor
+*/
 include "$system/source/solar/config/default.php";
-include "$system/source/proxima/config/default.php";
 
 /**
- * project overrides
- */
-
-// database connection
-$config['Solar_Sql']['adapter'] = 'Solar_Sql_Adapter_Sqlite';
-$config['Solar_Sql_Adapter_Sqlite'] = array(
-    'name' => "$system/sqlite/proxima.sq3",
-);
+* project overrides
+*/
 
 // front controller
 $config['Solar_Controller_Front'] = array(
-    'classes' => array('Proxima_App'),
+    'classes' => array('Solar_App'),
     'disable' => array(),
-    'default' => 'bookmarks',
+    'default' => 'hello',
     'rewrite' => array(),
     'routing' => array(),
     'explain' => true,
 );
 
 // model catalog
-$config['Solar_Sql_Model_Catalog']['classes'] = array('Proxima_Model');
-
-// authentication
-$config['Solar_Auth']['storage'] = array(
-    'adapter' => 'Solar_Auth_Storage_Adapter_Sql',
-);
-
-$config['Solar_Auth_Storage_Adapter_Sql'] = array(
-    'table'         => 'members',
-    'handle_col'    => 'handle',
-    'passwd_col'    => 'passwd',
-    'moniker_col'   => 'moniker',
-    'uri_col'       => 'uri',
-    'email_col'     => 'email',
-    'uid_col'       => 'id',
-    'hash_algo'     => 'md5',
-    'where'         => array('members.status = ?' => 'active'),
-);
-
-// access control
-$config['Solar_Access']['adapter'] = 'Solar_Access_Adapter_File';
-$config['Solar_Access_Adapter_File'] = array(
-    'file' => "$system/source/proxima/config/access.txt",
-);
+$config['Solar_Sql_Model_Catalog']['classes'] = array();
 
 /**
- * done!
- */
+* done!
+*/
 return $config;
+
